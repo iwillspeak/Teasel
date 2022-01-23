2b199fa293e26c7d17e69fb7eced195deed28dd2
Will Speak <lithiumflame@gmail.com>
2022-01-23 15:18:21 +0000

    Move RedGreen Tree to Pyracantha
    Move the red green tree implementaiton into a sub-module for better
    isolation.

--- src/syntax/Syntax.ts
+++ src/syntax/Syntax.ts
@@ -1,13 +1,4 @@
-// import {RedNode} from './RedTree';
-
-/**
- * Syntax Kind
- *
- * Syntax kinds are opaque markers used to indiciate the type of a node or token
- * in the concrete syntax tree of red and green nodes. It is up to the higher
- * level 'typed' AST to give these kinds meanings.
- */
-export type SyntaxKind = number;
+// import {RedNode} from './pyracantha/RedTree';
 
 // const ELEMENT: number = 1
 // const COMMENT: number = 1
