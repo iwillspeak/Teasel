cb97e8220f85eb3b2665632b22fac4f247936eed
James Garbutt <43081j@users.noreply.github.com>
2022-01-30 15:34:58 +0000

    feat: add initial cli

---
+++ src/cli.ts
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+import yargs from 'yargs';
+import {hideBin} from 'yargs/helpers';
+import {Parser} from './parse/Parser.js';
+import {readFile} from 'fs/promises';
+
+yargs(hideBin(process.argv))
+  .command<{path: string}>(
+    ['parse <path>', '$0 <path>'],
+    'parse a HTML file and output the AST',
+    (args) => {
+      return args.positional('path', {
+        describe: 'path to the file'
+      });
+    },
+    async (argv) => {
+      const contents = await readFile(argv.path, {encoding: 'utf8'});
+      const result = Parser.parseText(contents);
+      console.log(result);
+    }
+  )
+  .parse();
