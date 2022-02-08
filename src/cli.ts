61014f9e034ac9a1bae8165dde9b7977311b56da
Will Speak <lithiumflame@gmail.com>
2022-02-08 07:51:33 +0000

    misc: CLI Options
    Add option to output timings, and disable the dumping of the tree to
    the output.

--- src/cli.ts
+++ src/cli.ts
@@ -6,19 +6,46 @@ import {readFile} from 'fs/promises';
 import {debugDump} from './syntax/pyracantha/Debug.js';
 
 yargs(hideBin(process.argv))
-  .command<{path: string}>(
+  .command<{path: string, dump: boolean, time: boolean}>(
     ['parse <path>', '$0'],
     'parse a HTML file and output the AST',
     (args) => {
       return args.positional('path', {
         describe: 'path to the file',
         type: 'string'
+      })
+      .option('dump', {
+        describe: 'Dump the parsed file to standard output.',
+        boolean: true,
+        default: true,
+      })
+      .option('time', {
+        describe: 'Emit timings for to standard output.',
+        boolean: true,
+        default: false,
       });
     },
     async (argv) => {
+      if (argv.time) {
+        console.time('overall');
+        console.time('read');
+      }
+
       const contents = await readFile(argv.path, {encoding: 'utf8'});
+      if (argv.time) {
+        console.timeEnd('read');
+        console.time('parse');
+      }
+
       const result = Parser.parseText(contents);
-      debugDump(result.root, (k) => SyntaxKinds[k]);
+      if (argv.time) {
+        console.timeEnd('parse');
+        console.timeEnd('overall');
+      }
+
+      if (argv.dump) {
+        debugDump(result.root, (k) => SyntaxKinds[k]);
+      }
     }
   )
   .parse();
